db79c9521068c108ea3234b76da964bc9e74594f
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-01-14 11:47:43 +0000

    added database columns

--- models/race.js
+++ models/race.js
@@ -5,14 +5,87 @@ const RaceSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    startdate: {
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    club: {
+        type: String,
+        required: true
+    },
+    series: {
+        type: String,
+        required: true
+    },
+    org: {
+        type: String,
+        required: false
+    },
+    tel: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: false
+    },
+    pdf: {
+        type: String,
+        required: false
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    rbft: {
+        type: Boolean,
+        required: false
+    },
+    rbfn: {
+        type: Boolean,
+        required: false
+    },
+    rbbt: {
+        type: Boolean,
+        required: false
+    },
+    rbbn: {
+        type: Boolean,
+        required: false
+    },
+    rc: {
+        type: Boolean,
+        required: false
+    },
+    handicap: {
+        type: String,
+        required: false
+    },
+    regOpen: {
         type: Date,
         required: false
     },
-    enddate: {
+    regClose: {
         type: Date,
         required: false
+    },
+    reg: {
+        type: Boolean,
+        required: false
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
+
 });
 
 const Race = mongoose.model('Race', RaceSchema);
