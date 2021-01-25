92d899710a4184f785d678ed97a34f508dc64d93
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-01-25 15:58:00 +0000

    create checkpoint

---
+++ models/checkpoint.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const CheckpointSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: false
+    }
+});
+
+const Checkpoint = mongoose.model('Checkpoint', CheckpointSchema);
+
+module.exports = Checkpoint;
