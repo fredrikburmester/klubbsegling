09980994479cac152bf4510ae85324f6643ad9b5
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-02-20 13:42:51 +0000

    updates and functions for result page

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,31 +1,86 @@
-function unRegisterForRace(id) {
+function unRegisterForRace(id, el) {
     axios.post('/un-register-for-race/' + id, {
         headers: {
-          
+
         }
-    }).then(response => { 
-        if(response.status == 200) {
-           alert("Du är nu avregistrerad")
-           user.races = user.races.filter(e => e !== id); // will return ['A', 'C']
-           getRaces(year, club, serie)
+    }).then(response => {
+        if (response.status == 200) {
+            
+
+            window.createNotification({
+                closeOnClick: false,
+                displayCloseButton: false,
+                positionClass: "nfc-bottom-right",
+                showDuration: 5000,
+                theme: 'warning'
+            })({
+                title: "Avregistrerad",
+                message: "Du är nu avregistrerad på tävlingen"
+            });
+            
+            user.races = user.races.filter(e => e !== id); // will return ['A', 'C']
+            el.innerHTML = "Registrera"
+            el.classList.remove("btn-danger")
+            el.classList.add("btn-primary")
+            el.addEventListener('click', function handler() { 
+                registerForRace(id, this)
+                this.removeEventListener('click', handler);
+            });
         } else {
-            alert("Nått gick snett!")
+
+            window.createNotification({
+                closeOnClick: false,
+                displayCloseButton: false,
+                positionClass: "nfc-bottom-right",
+                showDuration: 5000,
+                theme: 'error'
+            })({
+                title: "Tyvärr!",
+                message: response.data.response.errors[0].msg
+            });
         }
     }).catch(error => console.error(error));
 }
 
-function registerForRace(id) {
+function registerForRace(id, el) {
+    console.log(el)
     axios.post('/register-for-race/' + id, {
         headers: {
-          
+
         }
-    }).then(response => { 
-        if(response.status == 200) {
-            alert("Du är nu registrerad")
+    }).then(response => {
+        if (response.status == 200 && response.data.response.success == true) {
+            window.createNotification({
+                closeOnClick: false,
+                displayCloseButton: false,
+                positionClass: "nfc-bottom-right",
+                showDuration: 5000,
+                theme: 'success'
+            })({
+                title: "Registrerad",
+                message: "Du är nu registrerad på tävlingen"
+            });
+
             user.races.push(id)
-            getRaces(year, club, serie)
+            el.innerHTML = "Avregistrera"
+            el.classList.remove("btn-primary")
+            el.classList.add("btn-danger")
+            el.addEventListener('click', function handler() { 
+                unRegisterForRace(id, this)
+                this.removeEventListener('click', handler);
+            });
         } else {
-            alert("Nått gick snett!")
+            console.log(response.data.response.errors)
+            window.createNotification({
+                closeOnClick: false,
+                displayCloseButton: false,
+                positionClass: "nfc-bottom-right",
+                showDuration: 5000,
+                theme: 'error'
+            })({
+                title: "Tyvärr!",
+                message: response.data.response.errors[0].msg
+            });
         }
     }).catch(error => console.error(error));
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const router  = express.Router();
-const {ensureAuthenticated} = require('../config/auth') 
+const router = express.Router();
+const {
+    ensureAuthenticated
+} = require('../config/auth')
 const User = require("../models/user");
 const Race = require("../models/race");
 const Club = require("../models/club");
@@ -8,79 +10,160 @@ const Serie = require("../models/serie");
 const Handicap = require("../models/handicap");
 const Checkpoint = require("../models/checkpoint");
 
-router.get('/', (req,res)=>{
-    res.redirect('/dashboard');
+function getTodaysDate(offset) {
+    var dateString = ""
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    dateString = (yyyy + offset) + "-" + mm + "-" + dd;
+
+    return dateString
+}
+
+router.get('/', ensureAuthenticated, async (req, res) => {
+
+    const races = await Race.find({
+        endDate: {
+            $gte: getTodaysDate(0),
+            $lte: getTodaysDate(1)
+        }
+    }).sort({
+        startDate: 1
+    });
+
+    res.render('dashboard', {
+        user: req.user,
+        races: races,
+    });
 })
 
 //register page
-router.get('/register', (req,res)=>{
+router.get('/register', (req, res) => {
     res.render('register');
 })
 
-router.post('/register-for-race/:id', async (req,res)=>{
+router.post('/register-for-race/:id', async (req, res) => {
     id = req.params.id
-    if(req.user === undefined) {
+    let errors = []
+
+    if (req.user === undefined) {
         res.status(401).end();
     } else {
 
         found = await Race.findById(id)
 
-        if (found) {
-            Race.updateOne(
-                { _id: id },
-                { $addToSet: { participants: {id: req.user.id, name: req.user.name}} },
-                function(err, result) {
-                  if (err) {
-                    res.status(500).end();
-                  } else {
-                    User.updateOne(
-                        { _id: req.user._id },
-                        { $addToSet: { races: id } },
-                        function(err, result) {
-                          if (err) {
-                            res.status(500).end();
-                          } else {
-                            res.status(200).end();
-                          }
+        var today = new Date();
+
+        // if race has been - no register
+        if(found.regClosed < today) {
+            errors.push({
+                msg: "Registreringen har tyvärr stängt"
+            })
+        } else if (found.endDate < today) {
+            errors.push({
+                msg: "Den här tävlingen har redan varit!"
+            })
+        } else if (found.startDate < today && found.endDate > today) {
+            errors.push({
+                msg: "Den här tävlingen är pågående!"
+            })
+        }
+    
+        
+
+        if (found && errors.length == 0) {
+            Race.updateOne({
+                    _id: id
+                }, {
+                    $addToSet: {
+                        participants: {
+                            id: req.user.id,
+                            name: req.user.name
                         }
-                      );
-                  }
+                    }
+                }, {useFindAndModify: false},
+                function (err, result) {
+                    if (err) {
+                        console.log(err)
+                        res.status(500).end();
+                    } else {
+                        User.updateOne({
+                                _id: req.user._id
+                            }, {
+                                $addToSet: {
+                                    races: id
+                                }
+                            }, {useFindAndModify: false},
+                            function (err, result) {
+                                if (err) {
+                                    console.log(err)
+                                    res.status(500).end();
+                                } else {
+                                    response = {
+                                        errors: errors,
+                                        success: true,
+                                    }
+                                    res.status(200)
+                                    res.setHeader('Content-Type', 'application/json');
+                                    res.end(JSON.stringify({ response }));
+                                }
+                            }
+                        );
+                    }
                 }
-              );
+            );
         } else {
-            console.log("not found")
-            res.status(404).end();
+            response = {
+                errors: errors,
+                success: false,
+            }
+            res.status(200)
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ response }));
         }
     }
 })
 
-router.post('/un-register-for-race/:id', async (req,res)=>{
+router.post('/un-register-for-race/:id', async (req, res) => {
     id = req.params.id
     console.log(id)
-    if(req.user === undefined) {
+    if (req.user === undefined) {
         res.status(401).end();
     } else {
 
         found = await Race.findById(id)
 
         if (found) {
-            await Race.findByIdAndUpdate(
-                { _id: id },
-                { $pull: { "participants": { id: req.user.id } } },
-                function(err, result) {
-                  if (err) {
-                    console.log("Update race: ",err)
-                    res.status(500).end();
-                  } else {
-                    console.log("participant removed")
-                  }
+            await Race.findByIdAndUpdate({
+                    _id: id
+                }, {
+                    $pull: {
+                        "participants": {
+                            id: req.user.id
+                        }
+                    }
+                },
+                function (err, result) {
+                    if (err) {
+                        console.log("Update race: ", err)
+                        res.status(500).end();
+                    } else {
+                        console.log("participant removed")
+                    }
                 }
             );
 
-            await User.findByIdAndUpdate(
-                { _id: req.user.id },
-                { $pull: { "races": id } },
-                function(err, result) {
+            await User.findByIdAndUpdate({
+                    _id: req.user.id
+                }, {
+                    $pull: {
+                        "races": id
+                    }
+                },
+                function (err, result) {
                     if (err) {
                         console.log("Update user: ", err)
                         res.status(500).end();
@@ -97,12 +180,12 @@ router.post('/un-register-for-race/:id', async (req,res)=>{
     }
 })
 
-router.get('/results', async (req,res)=>{
+router.get('/results', async (req, res) => {
     const series = await Serie.find()
     const clubs = await Club.find()
     const races = await Race.find()
 
-    res.render('results',{
+    res.render('results', {
         user: req.user,
         clubs: clubs,
         series: series,
@@ -110,18 +193,13 @@ router.get('/results', async (req,res)=>{
     });
 })
 
-// Logged in pages
-router.get('/dashboard',ensureAuthenticated, async (req,res)=>{
+// // Logged in pages
+// router.get('/dashboard',ensureAuthenticated, async (req,res)=>{
 
-    const races = await Race.find()
 
-    res.render('dashboard',{
-        user: req.user,
-        races: races,
-    });
-})
+// })
 
-router.get('/profile',ensureAuthenticated, async (req,res)=>{
+router.get('/profile', ensureAuthenticated, async (req, res) => {
 
     // const person = await User.findOne({_id: req.user._id}).lean()
     // console.log(req.user)
@@ -130,11 +208,11 @@ router.get('/profile',ensureAuthenticated, async (req,res)=>{
     var srsStdMono = require('../public/srs-std-mono.json');
 
 
-    res.render('profile',{
+    res.render('profile', {
         user: req.user,
         boats: srsCertMono,
         stdBoats: srsStdMono
     });
 })
 
-module.exports = router; 
+module.exports = router;
